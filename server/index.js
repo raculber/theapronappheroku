0138ab0a46a05eb54e2e69495d160d3e5594d7f2
raculber <rculbert5678@gmail.com>
2021-12-12 23:28:09 -0600

    fix server error

--- server/index.js
+++ server/index.js
@@ -6,17 +6,18 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import path from "path";
 
-app.use(express.static(path.join(__dirname, "client", "build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", routes);
+
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 const PORT = process.env.PORT;
 
